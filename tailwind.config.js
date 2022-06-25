050828b520d381898f3539e5e419618ed07c8db8
takumi-uezono0 <uezono1998@gmail.com>
2022-06-25 12:50:54 +0900

    チュートリアル完了後

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "tutorial-bg": "url('../src/img/main-bg.jpeg')",
+      },
+    },
   },
   plugins: [],
 };
